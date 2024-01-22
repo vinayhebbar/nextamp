bba9cdc8862a27fcd745e8586c107b83de1359ca
vinayhebbar <vinayhebbar@gmail.com>
2024-01-22 15:46:45 +0530

    getting db data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,77 +1,38 @@
-import { generateServerClientUsingCookies } from '@aws-amplify/adapter-nextjs/api';
-import { cookies } from 'next/headers';
-import { revalidatePath } from 'next/cache';
-import * as mutations from '@/graphql/mutations';
-// 1. Add the queries as an import
+import * as React from "react";
+import { Amplify } from 'aws-amplify';
+import { generateClient } from 'aws-amplify/api';
+import config from '@/amplifyconfiguration.json';
 import * as queries from '@/graphql/queries';
 
-import config from '@/amplifyconfiguration.json';
+Amplify.configure(config);
 
-const cookiesClient = generateServerClientUsingCookies({
-  config,
-  cookies
-});
-
-async function createTodo(formData: FormData) {
-  'use server';
-  const data = await cookiesClient.graphql({
-    query: mutations.createTodo,
-    variables: {
-      input: {
-        name: formData.get('name')?.toString() ?? ''
-      }
-    }
-  });
-
-
-  console.log('Created Todo: ', JSON.stringify(data));
-
-  revalidatePath('/');
-}
+const client = generateClient();
 
 export default async function Home() {
-  // 2. Fetch additional todos
-  const data = await cookiesClient.graphql({
-    query: queries.listTodos
-  });
-
-  // if(data && data.listTodos && data.listTodos.item) {
-    // const todos = data.listTodos.items;
-  // }
-
-  console.log('get data todo list', JSON.stringify(data));
-
-  const todos: any[] =  [];
+// Simple query
 
 
+const res: any = await client.graphql({ query: queries.listTodos }) || [];
+const allTodos: any = res.data.listTodos;
+console.log(allTodos);
+// console.log(JSON.stringify(allTodos));
+ // result: { "data": { "listTodos": { "items": [/* ..... */] } } }
 
   return (
-    <div
-      style={{
-        maxWidth: '500px',
-        margin: '0 auto',
-        textAlign: 'center',
-        marginTop: '100px'
-      }}
-    >
-      <form action={createTodo}>
-        <input name="name" placeholder="Add a todo" />
-        <button type="submit">Add</button>
-      </form>
+    <div>
+      xdfgdxfg
+      {allTodos?.items?.length > 0 && allTodos?.items?.map((item: any) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
 
-      {/* 3. Handle edge cases & zero state & error states*/}
-      {todos.length === 0 && (
-        <div>
-          <p>No todos, please add one.</p>
-        </div>
-      )}
-
-      {/* 4. Display todos*/}
-      {/* <ul>
-        {todos.length > 0 && todos.map((todo) => {
-          return <li style={{ listStyle: 'none' }}>{todo.name}</li>;
-        })}
-      </ul> */}
     </div>
   );
+
+//export default index;
+
 }
+// Fetch a single record by its identifier
+//const oneTodo = await client.graphql({
+  //uery: queries.getTodo,
+ // variables: { id: 'some id' }
+//});
